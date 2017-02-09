58f2db6e44d52acc3210b269b985aaf9d6ad8442
lydstyl <lydstyl@gmail.com>
2017-02-09 12:54:03 +0100

    talented correction and basePath for img ok

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,23 @@
 $(function(){
     var slider1 = Object.create(LydSlider);
-    //slider1.initSlider('lydSlider', 'bar', 'imgBox');
-    // slider1.initSlider({
-    //     containerClass : 'lydSlider',
-    //     barClass : 'bar',
-    //     imgBoxClass : 'imgBox'
-    // });
-    var slider1 = Object.create(LydSlider);
     slider1.init({
         containerClass : 'lydSlider',
         sWidth : 1000,
         sHeight : 444,
         spaceBeforeArrow : 155,
+    });
+    var slider2 = Object.create(LydSlider);
+    slider2.init({
+        containerClass : 'lydSlider2',
+        sWidth : 1000,
+        sHeight : 200,
+        spaceBeforeArrow : 120,
+    });
+    var slider3 = Object.create(LydSlider);
+    slider3.init({
+        containerClass : 'lydSlider3',
+        sWidth : 1000,
+        sHeight : 210,
+        spaceBeforeArrow : 72,
     });
 });
--- public/script.js
+++ public/script.js
@@ -1,16 +1,17 @@
 $(function(){
-    var slider1 = Object.create(LydSlider);
-    slider1.init({
-        containerClass : 'lydSlider',
-        sWidth : 1000,
-        sHeight : 444,
-        spaceBeforeArrow : 155,
-    });
-    var slider2 = Object.create(LydSlider);
-    slider2.init({
-        containerClass : 'lydSlider2',
-        sWidth : 500,
-        sHeight : 222,
-        spaceBeforeArrow : 600,
-    });
+    // var slider1 = Object.create(LydSlider);
+    // slider1.init({
+    //     containerClass : 'lydSlider',
+    //     sWidth : 1000,
+    //     sHeight : 444,
+    //     spaceBeforeArrow : 155,
+    // });
+    // var slider2 = Object.create(LydSlider);
+    // slider2.init({
+    //     containerClass : 'lydSlider2',
+    //     sWidth : 1000,
+    //     sHeight : 444,
+    //     spaceBeforeArrow : 155,
+    // });
+
 });
